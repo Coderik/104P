7b2f37b4691009a39aaaccbfa1d8b5d5b745e0ce
Coderik <coderiks@gmail.com>
2013-08-28 16:36:52 +0200

    Small changes in confidence mask calculation and new capabilities for its visualization

--- Algorithm/visualization.cpp
+++ Algorithm/visualization.cpp
@@ -35,4 +35,38 @@ Image<float>* Visualization::mask_to_greyscale(const ImageMask &mask)
 }
 
 
+Sequence<float>* Visualization::probability_to_greyscale(const Sequence<float> &probabilities)
+{
+	Shape size = probabilities.get_size();
+	Sequence<float> *result = new Sequence<float>(size.size_x, size.size_y, size.size_t, 0.0);
+
+	for (unsigned int t = 0; t < size.size_t; t++) {
+		for (unsigned int y = 0; y < size.size_y; y++) {
+			for (unsigned int x = 0; x < size.size_x; x++) {
+				float value = fmax(0.0, fmin(1.0, probabilities.get_value(x, y, t)));
+				result->set_value(x, y, t, 255.0 * value);
+			}
+		}
+	}
+
+	return result;
+}
+
+
+Image<float>* Visualization::probability_to_greyscale(const Image<float> &probabilities)
+{
+	Shape size = probabilities.get_size();
+	Image<float> *result = new Image<float>(size.size_x, size.size_y, 0.0);
+
+	for (unsigned int y = 0; y < size.size_y; y++) {
+		for (unsigned int x = 0; x < size.size_x; x++) {
+			float value = fmax(0.0, fmin(1.0, probabilities.get_value(x, y)));
+			result->set_value(x, y, 255.0 * value);
+		}
+	}
+
+	return result;
+}
+
+
 
